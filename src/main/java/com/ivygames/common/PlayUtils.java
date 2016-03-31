c4cb7809944af57d7d503e5d0978e8777d12fed6
Novikov, Ruslan <ruslan.novikov@here.com>
2016-03-31 16:16:49 +0200

    common module created

---
+++ src/main/java/com/ivygames/common/PlayUtils.java
@@ -0,0 +1,23 @@
+package com.ivygames.common;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+
+public class PlayUtils {
+
+    private PlayUtils() {
+        // utils
+    }
+
+    public static void rateApp(Context context) {
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse(getPlayUrl(context)));
+        context.startActivity(intent);
+    }
+
+    public static String getPlayUrl(Context context) {
+        return "https://play.google.com/store/apps/details?id=" + context.getPackageName();
+    }
+
+}
