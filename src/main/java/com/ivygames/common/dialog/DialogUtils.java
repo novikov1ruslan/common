032dcac7157e77e4d8ff8d79c01b73b82f355812
Novikov, Ruslan <ruslan.novikov@here.com>
2016-07-12 12:10:29 +0200

    compilation fixed

---
+++ src/main/java/com/ivygames/common/dialog/DialogUtils.java
@@ -0,0 +1,55 @@
+package com.ivygames.common.dialog;
+
+import android.app.FragmentManager;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
+
+import com.ivygames.common.R;
+import com.ruslan.fragmentdialog.AlertDialogBuilder;
+import com.ruslan.fragmentdialog.FragmentAlertDialog;
+
+public final class DialogUtils {
+
+    private DialogUtils() {
+        // utils
+    }
+
+    public static FragmentAlertDialog newOkCancelDialog(int message, final Runnable command) {
+        return new AlertDialogBuilder().setMessage(message).setPositiveButton(R.string.ok, new OnClickListener() {
+
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                command.run();
+            }
+        }).setNegativeButton(R.string.cancel).create();
+    }
+
+    public static FragmentAlertDialog newOkCancelDialog(String message, final Runnable command) {
+        return new AlertDialogBuilder().setMessage(message).setPositiveButton(R.string.ok, new OnClickListener() {
+
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                command.run();
+            }
+        }).setNegativeButton(R.string.cancel).create();
+    }
+
+    public static FragmentAlertDialog newOkDialog(int message, final Runnable command) {
+        return new AlertDialogBuilder().setMessage(message).setPositiveButton(R.string.ok, new OnClickListener() {
+
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                command.run();
+            }
+        }).setOnCancelListener(new DialogInterface.OnCancelListener() {
+            @Override
+            public void onCancel(DialogInterface dialog) {
+                command.run();
+            }
+        }).create();
+    }
+
+    public static void showNote(FragmentManager fm, int msgId) {
+        FragmentAlertDialog.showNote(fm, FragmentAlertDialog.TAG, msgId);
+    }
+}
